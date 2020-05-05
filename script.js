d0825d1114feaae2261af3bb52031a3828a8c215
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-05-05 16:11:15 +0600

    displaying to the DOM and Add User button done

--- script.js
+++ script.js
@@ -29,4 +29,27 @@ async function getRandomUser() {
 // add new obj to data arr 
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+// update DOM 
+function updateDOM(providedData = data) {
+    //clear the main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners 
+addUserBtn.addEventListener('click', getRandomUser);
